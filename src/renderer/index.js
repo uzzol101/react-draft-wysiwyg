7c1fc79ac386261d3bc0811d958d5c0d32c45aca
moksedul <davepillowgood@gmail.com>
2020-09-24 08:51:55 +0600

    fixed image crashing with japanese typing

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,27 +1,49 @@
+import React from 'react'
 import Embedded from './Embedded';
-import getImageComponent from '../renderer/Image';
+import ImageComponent from '../renderer/Image';
+
+
+let allConfig;
+
+const Media = (props) => {
+  const entity = props.contentState.getEntity(
+    props.block.getEntityAt(0)
+  );
+
+  const type = entity.getType();
+
+  let media;
+  if (type === 'EMBEDDED_LINK') {
+    media = <Embedded {...props}  />;
+  } else if (type === 'IMAGE') {
+    media = <ImageComponent {...props} config={allConfig} />;
+  }
+
+  return media;
+};
+
+function mediaBlockRenderer(block) {
+ if (block.getType() === 'atomic') {
+   return {
+     component: Media,
+     editable: false,
+   };
+ }
+
+ return '';
+}
+
 
 const getBlockRenderFunc = (config, customBlockRenderer) => (block) => {
+  
+  allConfig = config
+
   if (typeof customBlockRenderer === 'function') {
     const renderedComponent = customBlockRenderer(block, config, config.getEditorState);
     if (renderedComponent) return renderedComponent;
   }
-  if (block.getType() === 'atomic') {
-    const contentState = config.getEditorState().getCurrentContent();
-    const entity = contentState.getEntity(block.getEntityAt(0));
-    if (entity && entity.type === 'IMAGE') {
-      return {
-        component: getImageComponent(config),
-        editable: false,
-      };
-    } else if (entity && entity.type === 'EMBEDDED_LINK') {
-      return {
-        component: Embedded,
-        editable: false,
-      };
-    }
-  }
-  return undefined;
+
+  return mediaBlockRenderer(block)
 };
 
 export default getBlockRenderFunc;
